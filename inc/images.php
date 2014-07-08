452aab8baad8e72d2acca321a5fc6b109125250b
Joe Li <Joe@Joes-MacBook-Pro.local>
2014-07-08 10:57:37 +0800

    First commit for simplified 68a version, with no individual projects

---
+++ inc/images.php
@@ -0,0 +1,75 @@
+<?php 
+
+$images = array();
+
+	$images[1] = array(
+		"img-thumb" => "img/thumbs/project1.png",
+		"img-main" => "img/projects/project1.jpg",
+		"link" => "projects/project-1.php",
+		"title" => "Project 1",
+		"description" => "Traditional Bespoke",
+		"class" => "bespoke"
+	);
+
+	$images[2] = array(
+		"img-thumb" => "img/thumbs/project2.png",
+		"img-main" => "img/projects/project2.jpg",
+		"link" => "projects/project-2.php",
+		"title" => "Project 2",
+		"description" => "Bedrooms",
+		"class" => "bedrooms"
+	);
+
+	$images[3] = array(
+		"img-thumb" => "img/thumbs/project3.png",
+		"img-main" => "img/projects/project3.jpg",
+		"link" => "projects/project-3.php",
+		"title" => "Project 3",
+		"description" => "Modern & Handless",
+		"class" => "modern"
+	);
+	$images[4] = array(
+		"img-thumb" => "img/thumbs/project4.png",
+		"img-main" => "img/projects/project4.jpg",
+		"link" => "projects/project-4.php",
+		"title" => "Project 4",
+		"description" => "Traditional Bespoke",
+		"class" => "bespoke"
+	);
+	$images[5] = array(
+		"img-thumb" => "img/thumbs/project5.png",
+		"img-main" => "img/projects/project5.jpg",
+		"link" => "projects/project-5.php",
+		"title" => "Project 1",
+		"description" => "Traditional Bespoke",
+		"class" => "bespoke"
+	);
+
+	$images[6] = array(
+		"img-thumb" => "img/thumbs/project6.png",
+		"img-main" => "img/projects/project6.jpg",
+		"link" => "projects/project-6.php",
+		"title" => "Project 6",
+		"description" => "Bedrooms",
+		"class" => "bedrooms"
+	);
+
+	$images[7] = array(
+		"img-thumb" => "img/thumbs/project7.png",
+		"img-main" => "img/projects/project7.jpg",
+		"link" => "projects/project-7.php",
+		"title" => "Project 7",
+		"description" => "Modern & Handless",
+		"class" => "modern"
+	);
+	$images[8] = array(
+		"img-thumb" => "img/thumbs/project8.png",
+		"img-main" => "img/projects/project8.jpg",
+		"link" => "projects/project-8.php",
+		"title" => "Project 8",
+		"description" => "Traditional Bespoke",
+		"class" => "bespoke"
+	);
+
+
+?>
